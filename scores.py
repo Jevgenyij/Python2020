c0cc6590ca707a9aca188dd6509843a872e7adb3
Eugene Iurkevych <eugeneiurkevych@Eugenes-MacBook-Pro.local>
2020-05-23 10:55:03 +0200

    scores

---
+++ scores.py
@@ -0,0 +1,86 @@
+#scores = []
+#scores.append(72)
+#scores.append(100)
+#scores.append(100)
+
+#print(f"Average: {sum(scores) / len(scores)}")
+
+
+
+#from sys import exit
+
+#names = ["EMMA", "RODRIGO", "BRIAN", "DAVID"]
+    
+#if "RODRIGO" is names:
+        #print("Found")
+        #exit(0)
+#print("No found") 
+#exit(1)
+
+
+
+
+
+#from sys import exit 
+
+#people = {
+    #"Serega": "777-666-555",
+    #"Eugene": "888-777-666",
+    #"Alena": "999-000-111"
+#}
+#if "Serega" in people:
+    #print(f"Found {people['Serega']}")
+    #exit(0)
+#print("Not Found")
+#exit(1)
+
+
+#x = 1 * 2 
+#y = 2 * 5
+#z = 4 / 2
+#s = 8 / 4
+
+#print(f"x is {x}, y is {y}, z is {z}, s is {s}")
+#x, y, z, s = y, s, x, z
+#print(f"x is {x}, y is {y}, z is {z}, s is {s}")
+
+#import re
+
+#name = input("Do you agree?")
+
+#if re.search("y(es)?", name, re.IGNORECASE):
+       #print("Agree")
+#elif re.search("n(o)?", name):
+       #print("No Agree")
+       
+       
+       
+    #loop
+    
+#pizzas = {
+       
+    #"pizza": 10,
+    #"pasta": 12,
+    
+    #}
+#for pie, price in pizzas.items():
+    #print("A whole {} pizza cost ${}". format(pie, price))
+
+
+
+from time import sleep
+
+for i in range(0000, 100000):
+    if i == 1500:
+            
+print(f"Checking {i:04}")
+    
+    
+    
+
+
+#with open("large", "r") as file:
+    #for word in file.readlines():
+        #print(f"Checking {word.rsplit()}...")
+        #sleep(.1)
+        
